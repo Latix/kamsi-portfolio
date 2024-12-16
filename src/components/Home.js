79474d954334c6f01619749c88f1ce9c2bd03ab1
Kamsi Kodi <kamsikodi@gmail.com>
2024-12-16 09:21:35 +0100

    feat: Code Refactored

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,36 +1,36 @@
-import React, { useRef } from 'react' 
+import React, { useRef } from 'react'
 import CustomHook from './CustomHook';
 function Home() {
-const scrollTab = useRef();
-CustomHook(scrollTab);
+    const scrollTab = useRef();
+    CustomHook(scrollTab);
 
-return (
-    <section id="Home" ref={scrollTab} className='home'>
-        <div className="content">
-            <div className="name">
-                MY NAME IS <span>KAMSI KODI</span>
+    return (
+        <section id="Home" ref={scrollTab} className='home'>
+            <div className="content">
+                <div className="name">
+                    MY NAME IS <span>KAMSI KODI</span>
+                </div>
+                <div className="des">
+                    {/* 30 */}
+                    I’m a Full Stack Engineer with over 6 years of experience building and scaling web applications in industries like Fintech, HealthTech, and E-commerce. I specialize in creating efficient, user-focused solutions with modern JavaScript frameworks like React.js and Next.js, paired with backend technologies such as Flask, Node and Laravel. I’m skilled in strategic planning, integrating AI tools, and following best practices to deliver smooth, impactful user experiences. Whether it’s working with APIs, databases like MySQL, or cloud platforms like AWS and GCP, I focus on developing systems that are both robust and scalable. I’m passionate about driving innovation, whether it’s through automation, Web3, or AI-powered applications.
+                </div>
+
+                <a href="https://drive.google.com/file/d/1Fzwy8IAEy-YjiF6RXJ8lybKsdksCkBne/view?usp=sharing" target="_blank" rel="noopener noreferrer" className='animation active '>
+                    Download My CV
+                </a>
             </div>
-            <div className="des">
-                {/* 30 */}
-                Full Stack Engineer with 6 years of experience developing and maintaining highly scalable applications in the Fintech and E-commerce industries. Skilled in strategic planning and utilising best practices and industry standards to enhance user experiences in web applications leading to an increase in customer satisfaction. 
-            </div>
-            
-            <a href="https://drive.google.com/file/d/1VW6CcG9BG6N0wr3r5WwE1yRAeHrRzB3J/view?usp=sharing" target="_blank" rel="noopener noreferrer" className='animation active '>
-                Download My CV
-            </a>
-        </div>
-        <div className="avatar">
-            <div className="card">
-                <img src="/avatar1.png" alt="Kamsi Kodi" />
-                {/* <div className="info">
+            <div className="avatar">
+                <div className="card">
+                    <img src="/avatar1.png" alt="Kamsi Kodi" />
+                    {/* <div className="info">
                     <div>Developer</div>
                     <div>VietNamese</div>
                     <div>03/12</div>
                     <div>Male</div>
                 </div> */}
+                </div>
             </div>
-        </div>
-    </section>
+        </section>
     )
 }
 
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,64 +1,72 @@
-import React, { useState, useRef } from 'react' 
+import React, { useState, useRef } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPersonCircleQuestion, faEarthAmericas } from '@fortawesome/free-solid-svg-icons';
 import CustomHook from './CustomHook';
 
 function Projects() {
-const [listProjects] = useState([
-  {
-    name: 'SIMROP (SIM Registration Operations Portal)',
-    des: 'SIMROP is a management portal that allows MTN and their outlets to easily manage all SIM registration activities of agents and dealers in one place.An extension of BioSmart, the portal helps you monitor and control agents, track devices & kits, and manage customer information from anywhere.',
-    role: 'Software Engineer, Automation Tester',
-    language: 'HTML5, CSS3, Selenium, Java, TDD, Jenkins',
-    images: '/simrop.png',
-    link: 'https://sraa.mtnnigeria.net/simrop/'
-  },
-  {
-    name: 'Woven Finance',
-    des: 'Woven Finance simplifies day-to-day business transactions, making digital payments and reconciliations super easy for your business.',
-    role: 'Software Engineer',
-    language: 'HTML5, CSS3, Javascript, ReactJS, NodeJS, CI/CD, VueJS',
-    images: '/woven.png',
-    link: 'https://woven.finance/'
-  },
-  {
-    name: 'Workwise Visitor Management',
-    des: 'Whether you’re managing a small office or a large corporate campus, wVisitor has you covered. Say goodbye to the hassle of manual visitor logs and hello to a smarter, more streamlined way of managing visitors with wVisitor.',
-    role: 'Frontend Developer',
-    language: 'HTML5, CSS3, Javascript, React JS, Laravel, PHP, CI/CD',
-    images: '/workwise.png',
-    link: 'https://workwise.ng/'
-  },
-  {
-    name: 'Netflix Clone',
-    des: 'This is a simple netflix clone in react',
-    role: 'Frontend-developer',
-    language: 'HTML5, CSS3, ReactJS',
-    images: '/netflix.png',
-    link: 'https://silly-joliot-0ac098.netlify.app/'
-  },
-  {
-    name: 'Gzero',
-    des: 'GZERO Media is a company dedicated to providing the public with intelligent and engaging coverage of global affairs. It was created in 2017 as a subsidiary of Eurasia Group, the world\'s leading political risk analysis firm.',
-    role: 'Frontend-developer',
-    language: 'HTML5, CSS3, VanillaJS, API, Jinja',
-    images: '/gzero.png',
-    link: 'https://www.gzeromedia.com/'
-  },
-]);
-const divs = useRef([]);
-const scrollTab = useRef();
-CustomHook(scrollTab, divs);
+  const [listProjects] = useState([
+    {
+      name: 'Infiuss Probe',
+      des: 'An AI-driven platform revolutionising clinical research by connecting researchers, patients, and healthcare facilities. With a lean team of under 50 employees, Infiuss empowers researchers with cutting-edge tools for study planning, participant recruitment, and data management, driving innovation and expanding access to vital healthcare insights across the continent.',
+      role: 'AI Fullstack Engineer',
+      language: 'Javascript, Python, Nuxt.JS, Next.JS, MySQL, Laravel, Node',
+      images: '/probe.png',
+      link: 'https://infiuss.com'
+    },
+    {
+      name: 'Woven Finance',
+      des: 'Woven Finance simplifies day-to-day business transactions, making digital payments and reconciliations super easy for your business.',
+      role: 'Software Engineer',
+      language: 'HTML5, CSS3, Javascript, ReactJS, NodeJS, CI/CD, VueJS',
+      images: '/woven.png',
+      link: 'https://woven.finance/'
+    },
+    {
+      name: 'SIMROP (SIM Registration Operations Portal)',
+      des: 'SIMROP is a management portal that allows MTN and their outlets to easily manage all SIM registration activities of agents and dealers in one place.An extension of BioSmart, the portal helps you monitor and control agents, track devices & kits, and manage customer information from anywhere.',
+      role: 'Software Engineer, Automation Tester',
+      language: 'HTML5, CSS3, Selenium, Java, TDD, Jenkins',
+      images: '/simrop.png',
+      link: 'https://sraa.mtnnigeria.net/simrop/'
+    },
+    {
+      name: 'Workwise Visitor Management',
+      des: 'Whether you’re managing a small office or a large corporate campus, wVisitor has you covered. Say goodbye to the hassle of manual visitor logs and hello to a smarter, more streamlined way of managing visitors with wVisitor.',
+      role: 'Frontend Developer',
+      language: 'HTML5, CSS3, Javascript, React JS, Laravel, PHP, CI/CD',
+      images: '/workwise.png',
+      link: 'https://workwise.ng/'
+    },
+    {
+      name: 'Netflix Clone',
+      des: 'This is a simple netflix clone in react',
+      role: 'Frontend-developer',
+      language: 'HTML5, CSS3, ReactJS',
+      images: '/netflix.png',
+      link: 'https://silly-joliot-0ac098.netlify.app/'
+    },
+    {
+      name: 'Gzero',
+      des: 'GZERO Media is a company dedicated to providing the public with intelligent and engaging coverage of global affairs. It was created in 2017 as a subsidiary of Eurasia Group, the world\'s leading political risk analysis firm.',
+      role: 'Frontend-developer',
+      language: 'HTML5, CSS3, VanillaJS, API, Jinja',
+      images: '/gzero.png',
+      link: 'https://www.gzeromedia.com/'
+    },
+  ]);
+  const divs = useRef([]);
+  const scrollTab = useRef();
+  CustomHook(scrollTab, divs);
   return (
     <section id="Projects" className='projects' ref={scrollTab}>
-       <div className="title" ref={(el) => el && divs.current.push(el)}>
+      <div className="title" ref={(el) => el && divs.current.push(el)}>
         Projects
-       </div>
-       <div className="des" ref={(el) => el && divs.current.push(el)}>
+      </div>
+      <div className="des" ref={(el) => el && divs.current.push(el)}>
         {/* 20 */}
         {/* Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam perspiciatis quae veniam amet nesciunt voluptatibus quis consectetur consequatur quisquam harum. */}
-       </div>
-       <div className="list">
+      </div>
+      <div className="list">
         {
           listProjects.map((value, key) => (
             <div className='item' key={key} ref={(el) => el && divs.current.push(el)}>
@@ -71,24 +79,24 @@ CustomHook(scrollTab, divs);
                 <h3>{value.name}</h3>
                 <div className="des">{value.des}</div>
                 <div className="mission">
-                    <div><FontAwesomeIcon icon={faPersonCircleQuestion} /></div>
-                    <div>
-                        <h4>Role</h4>
-                        <div className="des">{value.role}</div>
-                    </div>
+                  <div><FontAwesomeIcon icon={faPersonCircleQuestion} /></div>
+                  <div>
+                    <h4>Role</h4>
+                    <div className="des">{value.role}</div>
+                  </div>
                 </div>
                 <div className="mission">
-                    <div><FontAwesomeIcon icon={faEarthAmericas} /></div>
-                    <div>
-                        <h4>Languages</h4>
-                        <div className="des">{value.language}</div>
-                    </div>
+                  <div><FontAwesomeIcon icon={faEarthAmericas} /></div>
+                  <div>
+                    <h4>Languages</h4>
+                    <div className="des">{value.language}</div>
+                  </div>
                 </div>
               </div>
             </div>
           ))
         }
-       </div>
+      </div>
     </section>
   )
 }
