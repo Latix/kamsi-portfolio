951536bff2709922b12023d60a66e4417121e424
Kamsi Kodi <kamsikodi@gmail.com>
2025-01-11 12:33:46 +0100

    patch: Updated portfolio UI

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,7 +16,7 @@ function Home() {
                     I’m a Full Stack Engineer with over 6 years of experience building and scaling web applications in industries like Fintech, HealthTech, and E-commerce. I specialize in creating efficient, user-focused solutions with modern JavaScript frameworks like React.js and Next.js, paired with backend technologies such as Flask, Node and Laravel. I’m skilled in strategic planning, integrating AI tools, and following best practices to deliver smooth, impactful user experiences. Whether it’s working with APIs, databases like MySQL, or cloud platforms like AWS and GCP, I focus on developing systems that are both robust and scalable. I’m passionate about driving innovation, whether it’s through automation, Web3, or AI-powered applications.
                 </div>
 
-                <a href="https://drive.google.com/file/d/1ggW1lSNjP9ZBovuallZxjf1395QDqhk3/view?usp=sharing" target="_blank" rel="noopener noreferrer" className='animation active '>
+                <a href="https://drive.google.com/file/d/1OEeUiAwe2fGbkKyutvNnDXbMZ0tHO64L/view?usp=sharing" target="_blank" rel="noopener noreferrer" className='animation active '>
                     Download My CV
                 </a>
             </div>
